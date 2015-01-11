94ef059e2743b0fe2ac895a4d4b8993f6e4d0668
Mikko Heikelä <mikko.heikela@iki.fi>
2015-01-11 18:14:30 +0200

    Added a rough attempt at Plot1 & Plot2

---
+++ Plot2.R
@@ -0,0 +1,14 @@
+# Load the whole dataset.
+# Assumes the data has been downloaded and the package unzipped manually
+# into the working directory.
+data <- read.csv("household_power_consumption.txt",
+                 header = TRUE,
+                 sep=";",
+                 na.strings = "?",
+                 colClasses=c("character", "character", rep("numeric",7)))
+
+# select the days we're interested in
+interesting_days <- data[data[["Date"]] == "1/2/2007" | data[["Date"]] == "2/2/2007",]
+
+# plot the time series
+plot(interesting_days[,"Global_active_power"],)
