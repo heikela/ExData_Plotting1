d2d073bab0ebe97738b2bb0db10cf3a23d8e9bb3
Mikko Heikelä <mikko.heikela@iki.fi>
2015-01-11 18:21:51 +0200

    Better formatting for Plot1

--- Plot1.R
+++ Plot1.R
@@ -10,5 +10,8 @@ data <- read.csv("household_power_consumption.txt",
 # select the days we're interested in
 interesting_days <- data[data[["Date"]] == "1/2/2007" | data[["Date"]] == "2/2/2007",]
 
-# plot the histogram
-hist(interesting_days[,"Global_active_power"])
+# plot and configure the histogram
+hist(interesting_days[,"Global_active_power"],
+     col="red",
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)")
